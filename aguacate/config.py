1117e9e829b7e7484bee59399ea622c63f409738
ANDREY STEVEN <andreyleon53@gmail.com>
2025-08-04 22:35:47 -0500

    Backend listoo

--- aguacate/config.py
+++ aguacate/config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-super-segura'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/aguacate_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/aguacate_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
